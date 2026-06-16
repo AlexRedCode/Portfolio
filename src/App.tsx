16f089f3ce560934d17be8ffc72454210abb9e1e
AlexRedCode <alexredonpravos@gmail.com>
2026-06-16 18:44:50 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import WellcomePage from "./assets/Components/WellcomePage.tsx";
+import NavBar from "./assets/Components/NavBar.tsx";
+
+function App() {
+    const [page, setPage] = useState(0)
+    const sections:string[] = ["Inicio","Información Personal","Historial Académico","Proyectos"]
+    return (
+        <>
+            <NavBar sections={sections} page={page} setPage={setPage}/>
+            {page === 0 ? (<WellcomePage/>):(<h1>WIP ⚠️</h1>)}
+        </>
+    )
+}
+
+export default App
